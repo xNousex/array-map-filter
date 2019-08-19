9511c6dc3f44a2cf25694b4c6f323683a1d2ca44
xNousex <damien.rambourg@gmail.com>
2019-08-19 11:50:21 +0200

    Fix Filter exercices 4

--- filter/ex4.js
+++ filter/ex4.js
@@ -29,6 +29,10 @@ Sortie attendue:
  */
 
 function filterOnPrice(products, maxPrice) {
+    return products.filter(function (product) {
+        return product.price <= maxPrice;
+
+    }, maxPrice)
 }
 
 // Ne pas modifier l'export
